3b254639c52d58f273cbad3c6f5cba8127075fb4
Nik Sharpio <niksharpkings@gmail.com>
2022-09-03 21:12:02 -0700

    heroku push 3

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 const APP_PREFIX = 'my-site-cache-';
 const VERSION = 'v1';
 const CACHE_NAME = APP_PREFIX + VERSION;
-const DATA_CACHE_NAME = "data-cache-" + VERSION;
+const DATA_CACHE_NAME = `data-cache-${VERSION}`;
 
 const FILES_TO_CACHE = [
   "/",
@@ -21,69 +21,62 @@ const FILES_TO_CACHE = [
 ];
 
 // Cache resources
-self.addEventListener("install", function(event) {
+self.addEventListener("install", (e) => {
   // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log('installing cache : ' + CACHE_NAME)
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log(`installing cache : ${CACHE_NAME}`)
       return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
 // Respond with cached resources
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", (e) => {
   // cache all get requests to /api routes
-  if (event.request.url.includes("/api/")) {
-    event.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(event.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(event.request.url, response.clone());
-            }
-
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(event.request);
-          });
+  if (e.request.url.includes("/api/")) {
+    e.respondWith(
+      caches.open(DATA_CACHE_NAME).then(async cache => {
+        try {
+          const response = await fetch(e.request);
+          // If the response was good, clone it and store it in the cache.
+          if (response.status === 200) {
+            cache.put(e.request.url, response.clone());
+          }
+          return response;
+        } catch (err) {
+          return
+        }
       }).catch(err => console.log(err))
     );
 
     return;
   }
 
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
-          return caches.match("/");
-        }
-      });
-    })
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request).then((response) => {
+      if (response) {
+        return response;
+      } else if (e.request.headers.get("accept").includes("text/html")) {
+        // return the cached home page for all requests for html pages
+        return caches.match("/");
+      }
+    }))
   );
 });
 
 // Delete outdated caches
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       // `keyList` contains all cache names under your username.github.io
       // filter out ones that has this app prefix to create white list
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      })
+      const cacheKeepList = keyList.filter((key) => key.indexOf(APP_PREFIX))
       // add current cache name to white list
-      cacheKeeplist.push(CACHE_NAME);
+      cacheKeepList.push(CACHE_NAME);
 
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheKeeplist.indexOf(key) === -1) {
+      return Promise.all(keyList.map((key, i) => {
+        if (cacheKeepList.indexOf(key) === -1) {
           console.log('deleting cache : ' + keyList[i] );
           return caches.delete(keyList[i]);
         }
